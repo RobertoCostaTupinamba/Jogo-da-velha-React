26bebf63caffd0128c6ba976af2feaa65bcfcff9
RobertoCostaTupinamba <robertotupinamba@gmail.com>
2020-02-19 13:04:03 -0300

    Alterando entre X E O

--- src/index.js
+++ src/index.js
@@ -2,67 +2,93 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+
+function Square(props) {
+    return (
+        <button
+        className="square"
+        onClick={props.onClick}
+    >
+        {props.value}
+    </button>
+    )
+}
+
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
     }
-  }
-  
-  class Board extends React.Component {
+
+    handlleClick(i){
+        const squares = [...this.state.squares]//this.state.squares.slice(); //cria uma copia do array existente
+        squares[i] = this.state.xIsNext? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        }) // seta a nova copia ao invés de alterar o existente
+    }
+
     renderSquare(i) {
-      return <Square />;
+        const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        
+        return (
+        <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handlleClick(i)}
+        />
+        );
     }
-  
+
     render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
     }
-  }
-  
-  class Game extends React.Component {
+}
+
+class Game extends React.Component {
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
     }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
+}
+
+// ========================================
+
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
-  
+);
